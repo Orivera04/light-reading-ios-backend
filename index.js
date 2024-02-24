f5461c52b0eaf8f7cc3fecafb1c31d29338efdc3
orivera04 <riveramoreira04@gmail.com>
2024-02-24 00:44:23 -0600

    Feat: Add support to x-www-form-urlencoded.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/v1/auth', require('./routes/authRoutes'));
