339a222bb765f322577029aed8f2107c843b4dd7
Kender <kendermendoza591@outlook.com>
2024-03-07 21:01:55 +0100

    - change expired token

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,7 @@ const generateJWT = ( uid, name ) => {
     const payload = { uid, name };
 
     jwt.sign(payload, process.env.SECRET_JWT_SEED, {
-      expiresIn: '24h'
+      expiresIn: '7d'
     }, (err, token) => {
       if (err) {
         console.log(err);
